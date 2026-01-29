30fb6d04af1efefe8db3bd2c34ffc1b6e9c0d692
prerit0 <masihprerit@gmail.com>
2026-01-29 11:34:53 +0530

    Added skills file

---
+++ skills.py
@@ -0,0 +1,8 @@
+SKILLS = [
+    "python", "java", "sql", "excel", "power bi",
+    "tableau", "machine learning", "deep learning",
+    "data analysis", "statistics", "tensorflow",
+    "pandas", "numpy", "scikit-learn",
+    "communication", "problem solving",
+    "cloud", "aws", "azure"
+]
